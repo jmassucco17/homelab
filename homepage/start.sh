221ba639c40a34eb1c21c41be246e12a72bb81e9
James Massucco <jmassucco17@gmail.com>
2025-05-05 04:04:38 +0000

    tweaks to blog generator

--- homepage/start.sh
+++ homepage/start.sh
@@ -4,6 +4,9 @@ cd "$(dirname "$0")"
 echo "Shutting down containers..."
 sudo docker compose down
 
+echo "Generating blog HTML..."
+python ./generate_blog.py
+
 echo "Updating container images..."
 sudo docker compose build homepage
 
